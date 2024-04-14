9359d89b76d7a4234914b7609cf50af2f262334d
ahmednasserhu <ahmedd.nasser.hussein@gmail.com>
2024-04-14 07:15:20 +0200

    fix assets

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py collectstatic
 python manage.py migrate
--- project/settings.py
+++ project/settings.py
@@ -160,7 +160,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'assets'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets') # for deployment
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
