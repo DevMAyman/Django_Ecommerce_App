ecc7e94c761cd9afd40e2c7f44c5e68a61bfd2c4
ahmednasserhu <ahmedd.nasser.hussein@gmail.com>
2024-04-14 06:47:43 +0200

    Deploy the project on render

--- project/settings.py
+++ project/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','django-ecommerce-app-1.onrender.com']
 
 
 # Application definition
