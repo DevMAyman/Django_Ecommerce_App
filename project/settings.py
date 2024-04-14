6eeb157811fa4327010355e947d5ce57de0384e4
ahmednasserhu <ahmedd.nasser.hussein@gmail.com>
2024-04-14 10:37:04 +0200

    allow new origins

--- project/settings.py
+++ project/settings.py
@@ -184,8 +184,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "user.User"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://django-ecommerce-ui.vercel.app/"
+    'https://django-ecommerce-ui.vercel.app/'
 ]
 CORS_ALLOW_CREDENTIALS = True
-CSRF_COOKIES_SECURE=True
