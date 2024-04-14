37df50374bf3c5f42034410e184d67e5461535e7
ahmednasserhu <ahmedd.nasser.hussein@gmail.com>
2024-04-14 06:23:36 +0200

    deploy on render

--- project/settings.py
+++ project/settings.py
@@ -160,6 +160,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'assets'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
